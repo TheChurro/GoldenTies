f4e1032cb821c7a1973e5e40266790c01e0f9195
John Churay <thechurroextrodinare@gmail.com>
2020-01-31 20:45:43 -0600

    Upgrade unity to Unit 3

--- Assets/Scripts/Rope.cs
+++ Assets/Scripts/Rope.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Rope : MonoBehaviour
 {
+    public bool freezeFirstLink;
     public Rigidbody2D[] ropeSegments;
     public Joint2D[] ropeConnections;
     public int numLinks;
@@ -22,14 +23,24 @@ public class Rope : MonoBehaviour
                 this.gameObject.transform
             ).GetComponent<Rigidbody2D>();
         }
-        float height = 2 * ropeSegments[0].GetComponent<Collider2D>().bounds.extents.y;
+        float height = 2 * ropeSegments[0].gameObject.GetComponent<Collider2D>().bounds.extents.y;
+        print("Extents: " + ropeSegments[0].gameObject.GetComponent<Collider2D>().bounds.extents);
+        ropeConnections = new Joint2D[numLinks - 1];
         for (int i = 1; i < numLinks; i++) {
             ropeSegments[i].transform.position = this.gameObject.transform.position 
                                                 - new Vector3(0, i * height, 0);
             DistanceJoint2D newJoint = ropeSegments[i].gameObject.AddComponent<DistanceJoint2D>();
+            newJoint.autoConfigureConnectedAnchor = false;
+            newJoint.autoConfigureDistance = false;
             newJoint.connectedBody = ropeSegments[i - 1];
             newJoint.connectedAnchor = new Vector2(0, -height * anchorDistance / 2);
             newJoint.anchor = new Vector2(0, height * anchorDistance / 2);
+            newJoint.distance = height * lenience;
+            newJoint.maxDistanceOnly = true;
+            ropeConnections[i - 1] = newJoint;
+        }
+        if (freezeFirstLink) {
+            ropeSegments[0].constraints = RigidbodyConstraints2D.FreezeAll;
         }
     }
 
