15870a14234b34a8e3f4aecf529b5c0182251bfd
Alvf <33241910+Alvf@users.noreply.github.com>
2020-02-01 16:42:29 -0600

    McGuffins
    Added a McGuffin

---
+++ Assets/McGuffin.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class McGuffin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Destroy(this.gameObject);
+    }
+}
