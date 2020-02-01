a0746c4e1a560253a54b846ea945a6ff081f5ed3
John Churay <thechurroextrodinare@gmail.com>
2020-02-01 07:56:01 -0600

    Initial rope implementation

---
+++ Assets/Scripts/MoveSine.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSine : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float time;
+    public Vector2 moveHalfDistance;
+    public float movementTime;
+    private float movementTimeRatio;
+    private Vector3 moveDistances;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+        startPos = this.transform.position;
+        movementTimeRatio = 2 * Mathf.PI / movementTime;
+        moveDistances = new Vector3(moveHalfDistance.x, moveHalfDistance.y, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > movementTime) {
+            time -= movementTime;
+        }
+        float ratio = Mathf.Sin(movementTimeRatio * time);
+        this.transform.position = startPos + ratio * moveDistances;
+    }
+}
--- Assets/Scripts/Rope.cs
+++ Assets/Scripts/Rope.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Rope : MonoBehaviour
 {
-    public bool freezeFirstLink;
+    public bool attachLinkToThis;
     public Rigidbody2D[] ropeSegments;
     public Joint2D[] ropeConnections;
     public int numLinks;
-    // Fraction of distance between achor points in local space.
-    public float anchorDistance;
-    // Fraction of height allowed between links.
-    public float lenience;
     public GameObject link;
+    private float linkHeight = 0.0f;
+    private FixedJoint2D endConnection;
+    private FixedJoint2D thisConnection;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,33 +19,33 @@ public class Rope : MonoBehaviour
         for (int i = 0; i < numLinks; i++) {
             ropeSegments[i] = Instantiate(
                 link,
-                this.gameObject.transform
+                this.gameObject.transform.position,
+                Quaternion.identity
             ).GetComponent<Rigidbody2D>();
         }
-        float height = 2 * ropeSegments[0].gameObject.GetComponent<Collider2D>().bounds.extents.y;
-        print("Extents: " + ropeSegments[0].gameObject.GetComponent<Collider2D>().bounds.extents);
+        linkHeight = 2 * ropeSegments[0].gameObject.GetComponent<Collider2D>().bounds.extents.y;
+        float invYScale = 1 / ropeSegments[0].gameObject.transform.localScale.y;
         ropeConnections = new Joint2D[numLinks - 1];
         for (int i = 1; i < numLinks; i++) {
             ropeSegments[i].transform.position = this.gameObject.transform.position 
-                                                - new Vector3(0, i * height, 0);
-            DistanceJoint2D newJoint = ropeSegments[i].gameObject.AddComponent<DistanceJoint2D>();
+                                                - new Vector3(0, i * linkHeight, 0);
+            HingeJoint2D newJoint = ropeSegments[i].gameObject.AddComponent<HingeJoint2D>();
             newJoint.autoConfigureConnectedAnchor = false;
-            newJoint.autoConfigureDistance = false;
             newJoint.connectedBody = ropeSegments[i - 1];
-            newJoint.connectedAnchor = new Vector2(0, -height * anchorDistance / 2);
-            newJoint.anchor = new Vector2(0, height * anchorDistance / 2);
-            newJoint.distance = height * lenience;
-            newJoint.maxDistanceOnly = true;
+            newJoint.connectedAnchor = new Vector2(0, -0.5f * linkHeight);
+            newJoint.anchor = new Vector2(0, 0.5f * linkHeight);
+            newJoint.enableCollision = false;
+            JointAngleLimits2D limits2D = new JointAngleLimits2D();
+            limits2D.min = -Mathf.PI / 2;
+            limits2D.max = Mathf.PI / 2;
+            newJoint.limits = limits2D;
+            newJoint.useLimits = true;
             ropeConnections[i - 1] = newJoint;
         }
-        if (freezeFirstLink) {
-            ropeSegments[0].constraints = RigidbodyConstraints2D.FreezeAll;
+        if (attachLinkToThis) {
+            thisConnection = ropeSegments[0].gameObject.AddComponent<FixedJoint2D>();
+            thisConnection.connectedBody = this.GetComponent<Rigidbody2D>();
+            thisConnection.autoConfigureConnectedAnchor = false;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
