ed4b6a0dade2804a62efebf752d5f2101a2356e1
Alvf <33241910+Alvf@users.noreply.github.com>
2020-02-01 18:24:54 -0600

    UI polishing
    Ceramic numbers update!

--- Assets/McGuffin.cs
+++ Assets/McGuffin.cs
@@ -7,6 +7,9 @@ public class McGuffin : MonoBehaviour
     // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D col)
     {
+        var ceram = GameObject.Find("NumCeramics").GetComponent<CeramicIndicator>();
+        ceram.ceramicnumber++;
+        ceram.UpdateScore();
         Destroy(this.gameObject);
     }
 }
---
+++ Assets/Scripts/CeramicIndicator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CeramicIndicator : MonoBehaviour
+{
+    public int ceramicnumber;
+    public TextMeshProUGUI typer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScore();
+    }
+    public void UpdateScore()
+    {
+        typer.text = ceramicnumber.ToString();
+    }
+}
