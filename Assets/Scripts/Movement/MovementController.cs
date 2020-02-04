4f7bbd9d079c39a67f1259faa0feda4530a7ab7a
John Churay <thechurroextrodinare@gmail.com>
2020-02-04 14:08:54 -0600

    Incremental changes

--- Assets/Scripts/Movement/MovementController.cs
+++ Assets/Scripts/Movement/MovementController.cs
@@ -25,7 +25,9 @@ public class MovementController : RaycastMovement
 
     private struct SlopeTransition {
         public bool doTransition;
+        public bool useVerticalVelocityInsteadOfHorizontal;
         public float cosSlope;
+        public bool descend;
     }
 
     public void Move(Vector2 targetDisplacement, Vector2 input) {
@@ -47,6 +49,7 @@ public class MovementController : RaycastMovement
                     GetRayLine(side),
                     side,
                     true,
+                    false,
                     ref transition,
                     ref distance,
                     direction
@@ -60,6 +63,7 @@ public class MovementController : RaycastMovement
                     GetRayLine(side),
                     side,
                     false,
+                    side == RaycastBoundarySide.Bottom,
                     ref transition,
                     ref distance,
                     direction
@@ -74,9 +78,12 @@ public class MovementController : RaycastMovement
                 // Reflect the movement direction up the slope.
                 var newDirection = new Vector2(
                     transition.cosSlope * Mathf.Sign(direction.x),
-                    // Always up because only detecting bottom hits!
                     Mathf.Sqrt(Mathf.Max(0, 1 - transition.cosSlope * transition.cosSlope))
                 );
+                // if we are descending, flip the y
+                if (transition.descend) {
+                    newDirection.y *= -1;
+                }
                 // Don't move up a slope if we are moving away from it.
                 if (direction.y > newDirection.y) {
                     support.slopeCos = 1;
@@ -86,6 +93,11 @@ public class MovementController : RaycastMovement
                 direction = newDirection;
                 support.slopeCos = transition.cosSlope;
                 targetDistance -= distance;
+                if (transition.useVerticalVelocityInsteadOfHorizontal) {
+                    //targetDistance = Mathf.Abs()
+                } else {
+
+                }
             } else {
                 break;
             }
@@ -94,18 +106,17 @@ public class MovementController : RaycastMovement
         this.transform.Translate(displacement);
     }
 
-    private void DescendSlope() {
-
-    }
-
     private void DetectCollisions(
         RaycastBoundaryLine line,
         RaycastBoundarySide boundarySide,
         bool climbSlopes,
+        bool descendSlopes,
         ref SlopeTransition transition,
         ref float moveDistance,
         Vector2 direction
     ) {
+        // Check for descending slopes on the opposite direction of movement.
+        int descendCheckIndex = direction.x < 0 ? line.numRays - 1 : 0;
         for (int i = 0; i < line.numRays; i++) {
             Vector2 rayOrigin = line.origin + i * line.displacement;
             var hit = Physics2D.Raycast(rayOrigin, direction, moveDistance, collisionMask);
@@ -118,11 +129,22 @@ public class MovementController : RaycastMovement
                     case RaycastBoundarySide.Right: support.hitRight = true; break;
                 }
                 if (i == 0 && climbSlopes) {
-                    transition.cosSlope = Vector2.Dot(Vector2.up, hit.normal);
-                    if (transition.cosSlope >= cosMaxSlope) {
+                    var cosSlope = Vector2.Dot(Vector2.up, hit.normal);
+                    if (cosSlope >= cosMaxSlope) {
+                        transition.cosSlope = cosSlope;
                         transition.doTransition = true;
+                        transition.descend = false;
                         continue;
                     }
+                }
+                // We always descend down slopes. The only caveat is that we may
+                // not have control on those slopes
+                if (i == descendCheckIndex && descendSlopes) {
+                    transition.cosSlope = Vector2.Dot(Vector2.up, hit.normal);
+                    transition.doTransition = true;
+                    transition.useVerticalVelocityInsteadOfHorizontal = transition.cosSlope < cosMaxSlope;
+                    transition.descend = true;
+                    continue;
                 }
                 transition.doTransition = false;
             }
